fb84da07700c3eb2c689c7e022ff03b98bfee56a
PratikNichit1 <pratiknichit7@gmail.com>
2025-01-26 14:43:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    app.logger.info(f"Request received from {request.remote_addr}")
+    return "Python Apppppp Hosting"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
